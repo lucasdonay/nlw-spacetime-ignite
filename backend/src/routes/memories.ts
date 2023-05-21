e43cdee7a915c8133d5c3e3df3660a61b0296522
Donay <donay@ingressolive.com>
2023-05-21 00:48:00 -0300

    day 03

---
+++ backend/src/routes/memories.ts
@@ -0,0 +1,101 @@
+import { FastifyInstance } from 'fastify'
+import { z } from 'zod'
+import { prisma } from '../lib/prisma'
+
+export async function memoriesRoutes(app: FastifyInstance) {
+  app.get('/memories', async () => {
+    const memories = await prisma.memorys.findMany({
+      orderBy: {
+        createdAt: 'asc',
+      },
+    })
+
+    return memories.map((memorys) => {
+      return {
+        id: memorys.id,
+        coverUrl: memorys.coverUrl,
+        excerpt: memorys.content.substring(0, 110).concat('...'),
+      }
+    })
+  })
+
+  app.get('/memories/:id', async (request) => {
+    const paramsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = paramsSchema.parse(request.params)
+
+    const memory = await prisma.memorys.findUniqueOrThrow({
+      where: {
+        id,
+      },
+    })
+
+    return memory
+  })
+
+  app.post('/memories', async (request) => {
+    const bodySchema = z.object({
+      content: z.string(),
+      coverUrl: z.string(),
+      isPublic: z.coerce.boolean().default(false),
+    })
+
+    const { content, coverUrl, isPublic } = bodySchema.parse(request.body)
+
+    const memory = await prisma.memorys.create({
+      data: {
+        content,
+        coverUrl,
+        isPublic,
+        userId: '321af4ed-a235-4311-9cdf-c4f44d146a48',
+      },
+    })
+
+    return memory
+  })
+
+  app.put('/memories/:id', async (request) => {
+    const paramsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = paramsSchema.parse(request.params)
+
+    const bodySchema = z.object({
+      content: z.string(),
+      coverUrl: z.string(),
+      isPublic: z.coerce.boolean().default(false),
+    })
+
+    const { content, coverUrl, isPublic } = bodySchema.parse(request.body)
+
+    const memory = await prisma.memorys.update({
+      where: {
+        id,
+      },
+      data: {
+        content,
+        isPublic,
+        coverUrl,
+      },
+    })
+
+    return memory
+  })
+
+  app.delete('/memories/:id', async (request) => {
+    const paramsSchema = z.object({
+      id: z.string().uuid(),
+    })
+
+    const { id } = paramsSchema.parse(request.params)
+
+    await prisma.memorys.delete({
+      where: {
+        id,
+      },
+    })
+  })
+}
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,19 +1,19 @@
-import { fastify } from 'fastify'
-import { PrismaClient } from '@prisma/client'
+import fastisy from 'fastify'
+import { memoriesRoutes } from './routes/memories'
+import cors from '@fastify/cors'
 
-const app = fastify()
-const prisma = new PrismaClient()
+const app = fastisy()
 
-app.get('/users', async () => {
-  const users = await prisma.user.findMany()
-
-  return users
+app.register(cors, {
+  origin: true,
 })
+
+app.register(memoriesRoutes)
 
 app
   .listen({
     port: 3333,
   })
   .then(() => {
-    console.log('HTTP server running on http://localhost:3333 🚀')
+    console.log('HTTP server running on http://localhost:3333')
   })
